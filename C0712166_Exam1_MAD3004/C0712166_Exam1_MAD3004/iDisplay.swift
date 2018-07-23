63eca1b932114b14ee7c037f4212d830919c85ae
MacStudent <macstudent@20X-iMacXX-M-404.local>
2018-07-23 17:10:33 -0400

    test

---
+++ C0712166_Exam1_MAD3004/C0712166_Exam1_MAD3004/iDisplay.swift
@@ -0,0 +1,9 @@
+//
+//  iDisplay.swift
+//  C0712166_Exam1_MAD3004
+//
+//  Created by MacStudent on 2018-07-23.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
---
+++ C0712166_Exam1_MAD3004/C0712166_Exam1_MAD3004/main.swift
@@ -0,0 +1,39 @@
+//
+//  main.swift
+//  C0712166_Exam1_MAD3004
+//
+//  Created by MacStudent on 2018-07-23.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+var v1 = Product(ProductId:350,ProductName: "BMW", ProductPrice: 145.0, ProductQuantity: "15")
+
+
+
+var v2 = Product(ProductId:230,ProductName: "Mercidies", ProductPrice: 167.0, ProductQuantity: "50")
+
+
+
+var v3 = Product(ProductId:460,ProductName: "Honda", ProductPrice: 545.0, ProductQuantity: "60")
+
+
+
+var v4 = Product(ProductId:550,ProductName: "Hundayi", ProductPrice: 645.0, ProductQuantity: "30")
+
+
+
+var v5 = Product(ProductId:800,ProductName: "Mazada", ProductPrice: 123.0, ProductQuantity: "68")
+
+
+
+v1.display()
+
+v2.display()
+
+v3.display()
+
+v4.display()
+
+v5.display()
